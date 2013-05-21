fe5de1d28dde488cd061fa8a42c6127f2349be27
Paul Crompton <paul.david.crompton@me.com>
2013-05-21 14:06:15 -0400

    fixed merge error

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
-require 'test.php';
+<!DOCTYPE html>
+<html>
+<body>
+<form action="test.php" method="post">
+	<h3>Local MySQL Server</h3>
+	Server Name: <input type="text" name="sn"><br>
+	MySQL User Name: <input type="text" name="un"><br>
+	MySQL Password: <input type="text" name="pw"><br>
+	<br>
+
+	<input type="submit">
+</form>
+
+</body>
+</html>
 
 ?>
--- input_credentials.php
+++ input_credentials.php
@@ -1,20 +1,17 @@
 <?php
 
-$un = 'root';
-$pw = 'root';
-$srv = 'localhost';
+$inputun = $_POST["un"];
+$inputpw = $_POST["pw"];
+$inputsn = $_POST["sn"];
 
 $dbA = 'dbA';
 $dbB = 'dbB';
 $buf = 'buf';
 
-$input_cred = array($srv, $un, $pw);
-
-$dbA_cred = array($srv, $un, $pw, $dbA);
-$dbB_cred = array($srv, $un, $pw, $dbB);
-$buf_cred = array($srv, $un, $pw, $buf);
-
-
+$input_cred = array($inputsn, $inputun, $inputpw);
+$dbA_cred = array($inputsn, $inputun, $inputpw, $dbA);
+$dbB_cred = array($inputsn, $inputun, $inputpw, $dbB);
+$buf_cred = array($inputsn, $inputun, $inputpw, $buf);
 
 
 ?>
