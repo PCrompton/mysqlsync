2ac71c5b7bd563264a7400112e7c73cc9c9b75f0
Paul Crompton <paul.david.crompton@me.com>
2013-05-29 17:43:30 -0400

    pasted readme text in index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 require "links.html";
 ?>
+<br>
+mysqlsync<br>
+=========<br><br>
+
+Syncs MySQL databases<br><br>
+
+Requirements: <br>
+-MySQL 5.6 (previous versions untested)<br>
+-PHP 5.3 (previous versions untested)<br><br>
+
+Recommended:<br>
+-phpmyadmin 3.5<br><br>
+
+Instructions:<br><br>
+
+1. Download mysqlsync and place it on your local server.<br>
+2. In your browser's address bar, type localhost/mysqlsync (or whatever the filename may be).<br>
+3. Open 'input_credentials.php' with a text editor and replace the following strings with your own mysql credentials:<br>
+	$un = 'root';<br>
+	$pw = 'root';<br>
+	$srv = 'localhost';<br>
+4. (optional) log in to phpmyadmin before and after running the script to see changes more clearly<br><br>
